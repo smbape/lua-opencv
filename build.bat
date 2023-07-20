dd7b1a08191fd0a8b3e3f17862d5b9760ab0ef92
Stéphane MBAPE <smbape@yahoo.fr>
2023-07-20 16:06:55 +0200

    fix linux build

--- build.bat
+++ build.bat
@@ -10,8 +10,10 @@ SET skip_build=0
 SET skip_config=0
 SET has_prefix=0
 SET has_install=0
+SET has_source=0
 SET TARGET=ALL_BUILD
 SET CMAKE_GENERATOR=
+SET SOURCE=
 SET PREFIX=
 
 SET nparms=20
@@ -21,6 +23,13 @@ IF %nparms% == 0 GOTO :MAIN
 IF [%1] == [-g] SET skip_build=1
 IF [%1] == [--build] SET skip_config=1
 IF [%1] == [--install] SET has_install=1
+IF [%1] == [-C] (
+    SET has_source=1
+    SET SOURCE=%2
+    SET /a nparms -=1
+    SHIFT
+    IF %nparms% == 0 GOTO :MAIN
+)
 IF [%1] == [--prefix] (
     SET has_prefix=1
     SET PREFIX=%2
@@ -52,8 +61,9 @@ SHIFT
 GOTO GET_OPTS
 
 :MAIN
+IF [%has_source%] == [1] CD /D "%SOURCE%"
 SET BUILD_FOLDER=build_x64
-IF NOT [%has_prefix%] == [1] SET "PREFIX=%CWD%\%BUILD_FOLDER%\install"
+IF NOT [%has_prefix%] == [1] SET "PREFIX=%CD%\%BUILD_FOLDER%\install"
 
 IF NOT DEFINED CMAKE_BUILD_TYPE SET CMAKE_BUILD_TYPE=Release
 SET EXTRA_CMAKE_OPTIONS=%EXTRA_CMAKE_OPTIONS% -DCMAKE_BUILD_TYPE="%CMAKE_BUILD_TYPE%" -DCMAKE_INSTALL_PREFIX=install
