c053187efd1d9104fb251a40a0a695c45e2f2d4c
Stéphane MBAPE <smbape@yahoo.fr>
2023-07-22 09:02:33 +0200

    build on linux

--- build.bat
+++ build.bat
@@ -1,35 +1,43 @@
 @ECHO OFF
 SETLOCAL enabledelayedexpansion
 
+SET WORKING_DIR=%CD%
+
 PUSHD "%~dp0"
-CD /d %CD%
+CD /D %CD%
 SET "PATH=%CD%;%PATH%"
-SET "CWD=%CD%"
+SET "SCRIPTPATH=%CD%"
+
+CD /D "%WORKING_DIR%"
+SET BUILD_FOLDER=%CD%\build_x64
 
 SET skip_build=0
 SET skip_config=0
-SET has_prefix=0
 SET has_install=0
-SET has_source=0
+SET has_prefix=0
+SET is_dry_run=0
+SET has_test=0
 SET TARGET=ALL_BUILD
 SET CMAKE_GENERATOR=
-SET SOURCE=
-SET PREFIX=
+SET PREFIX=%BUILD_FOLDER%\install
 
 SET nparms=20
 
 :GET_OPTS
 IF %nparms% == 0 GOTO :MAIN
-IF [%1] == [-g] SET skip_build=1
-IF [%1] == [--build] SET skip_config=1
-IF [%1] == [--install] SET has_install=1
-IF [%1] == [-C] (
-    SET has_source=1
-    SET SOURCE=%2
-    SET /a nparms -=1
-    SHIFT
-    IF %nparms% == 0 GOTO :MAIN
+IF [%1] == [--dry-run] SET is_dry_run=1
+IF [%1] == [--no-build] SET skip_build=1
+IF [%1] == [--no-config] SET skip_config=1
+IF [%1] == [--test] SET has_test=1
+IF [%1] == [-g] (
+    SET skip_build=1
+    SET skip_config=0
 )
+IF [%1] == [--build] (
+    SET skip_build=0
+    SET skip_config=1
+)
+IF [%1] == [--install] SET has_install=1
 IF [%1] == [--prefix] (
     SET has_prefix=1
     SET PREFIX=%2
@@ -61,12 +69,8 @@ SHIFT
 GOTO GET_OPTS
 
 :MAIN
-IF [%has_source%] == [1] CD /D "%SOURCE%"
-SET BUILD_FOLDER=build_x64
-IF NOT [%has_prefix%] == [1] SET "PREFIX=%CD%\%BUILD_FOLDER%\install"
-
 IF NOT DEFINED CMAKE_BUILD_TYPE SET CMAKE_BUILD_TYPE=Release
-SET EXTRA_CMAKE_OPTIONS=%EXTRA_CMAKE_OPTIONS% -DCMAKE_BUILD_TYPE="%CMAKE_BUILD_TYPE%" -DCMAKE_INSTALL_PREFIX=install
+SET EXTRA_CMAKE_OPTIONS=%EXTRA_CMAKE_OPTIONS% -DCMAKE_BUILD_TYPE="%CMAKE_BUILD_TYPE%"
 
 ::Find CMake
 SET CMAKE="cmake.exe"
@@ -76,30 +80,36 @@ IF EXIST "%PROGRAMW6432%\CMake\bin\cmake.exe" SET CMAKE="%PROGRAMW6432%\CMake\bi
 
 :MAKE
 SET ERROR=0
+SET TRY_RUN=
+IF [%is_dry_run%] == [1] SET "TRY_RUN=@ECHO "
+IF [%is_dry_run%] == [1] SET "CMAKE=@ECHO %CMAKE%"
 
 :MAKE_CONFIG
-IF [%skip_config%] == [1] GOTO BUILD
-
 IF NOT EXIST %BUILD_FOLDER% MKDIR %BUILD_FOLDER%
-cd %BUILD_FOLDER%
+%TRY_RUN%CD /D "%BUILD_FOLDER%"
 
-%CMAKE% %CMAKE_GENERATOR% %EXTRA_CMAKE_OPTIONS% "%CWD%"
+IF [%skip_config%] == [1] GOTO BUILD
+%CMAKE% %CMAKE_GENERATOR% %EXTRA_CMAKE_OPTIONS% "%SCRIPTPATH%"
 SET ERROR=%ERRORLEVEL%
 IF [%ERROR%] == [0] GOTO BUILD
 GOTO END
 
 :BUILD
-IF [%skip_build%] == [1] GOTO END
-IF NOT [%CD%] == [%CWD%\%BUILD_FOLDER%] CD /D "%CWD%\%BUILD_FOLDER%"
+IF [%skip_build%] == [1] GOTO INSTALL
 %CMAKE% --build . --config %CMAKE_BUILD_TYPE% --target %TARGET%
 SET ERROR=%ERRORLEVEL%
 IF [%ERROR%] == [0] GOTO INSTALL
 GOTO END
 
 :INSTALL
-IF NOT [%has_install%] == [1] GOTO END
-IF NOT [%CD%] == [%CWD%\%BUILD_FOLDER%] CD /D "%CWD%\%BUILD_FOLDER%"
+IF NOT [%has_install%] == [1] GOTO TEST
 %CMAKE% --install . --prefix "%PREFIX%"
+IF [%ERROR%] == [0] GOTO TEST
+GOTO END
+
+:TEST
+IF NOT [%has_test%] == [1] GOTO END
+"%PREFIX%\bin\luajit.exe" "%SCRIPTPATH%\test\test.lua"
 
 :END
 POPD
---
+++ build.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+die() {
+    echo "ERROR: $1" 1>&2
+    [ $2 ] && exit $2 || exit 1
+}
+
+#Absolute path to this script
+SCRIPT=$(readlink -f "$0")
+#Absolute path this script is in
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+skip_build=0
+skip_config=0
+has_install=0
+is_dry_run=0
+has_test=0
+GENERATOR=Ninja
+PLATFORM=
+TARGET=all
+BUILD_FOLDER="$PWD/build_linux"
+PREFIX="$BUILD_FOLDER/install"
+
+print_help() {
+    echo '
+Usage: ./build.sh [options]
+
+Options:
+    --help|h|help               Display the help message.
+    --dry-run                   Display commands that will be executed without running them.
+    --g                         Generate cmake config only.
+    --no-config                 Do not generate cmake config.
+    --build                     Build only.
+    --no-build                  Do not build.
+    --install                   Install.
+    --test                      Execute test.
+    -G <generator-name>         Specify a build system generator. ('${GENERATOR}')
+    -A <platform-name>          Specify platform name if supported by generator. ('${PLATFORM}')
+    --prefx <directory>         Install directory. ('$PREFIX')
+    --target <target-name>      Target to build. ('$TARGET')
+'
+}
+
+ac_opt=0
+ac_prev=
+for ac_option in "$@"; do
+    # If the previous option needs an argument, assign it.
+    if test ${#ac_prev} -ne 0; then
+        eval "$ac_prev='$ac_option'"
+        ac_prev=
+        continue
+    fi
+
+    case "$ac_option" in
+        --dry-run)
+            is_dry_run=1
+            continue
+            ;;
+        -g)
+            skip_build=1
+            skip_config=0
+            continue
+            ;;
+        --no-config)
+            skip_config=1
+            continue
+            ;;
+        --build)
+            skip_build=0
+            skip_config=1
+            continue
+            ;;
+        --no-build)
+            skip_build=1
+            continue
+            ;;
+        --install)
+            has_install=1
+            continue
+            ;;
+        --test)
+            has_test=1
+            continue
+            ;;
+    esac
+
+    case "$ac_option" in
+        *=*)
+            ac_opt=0
+            key="${ac_option%%=*}"
+            value="${ac_option#*=}"
+            ;;
+        *)
+            ac_opt=1
+            key="${ac_option}"
+            value=
+            ;;
+    esac
+
+    case "$key" in
+        -C)
+            key=--binary-dir
+            ;;
+        -G)
+            key=--generator
+            ;;
+        -A)
+            key=--platform
+            ;;
+        --binary-dir|--generator|--platform )
+            echo "Unknown option $key" 1>&2
+            exit 1
+            ;;
+    esac
+
+    case "$key" in
+        --binary-dir|--generator|--platform|--prefix|--target )
+            key="${key:2}"
+            key="${key//-/_}"
+
+            if test $ac_opt -eq 1; then
+                ac_prev="${key^^}"
+            else
+                eval "${key^^}='${value}'"
+            fi
+            ;;
+        --help|-h|-help )
+            print_help
+            exit 0
+            ;;
+        * )
+            echo "Unknown option $key" 1>&2
+            exit 1
+            ;;
+    esac
+done
+
+try_run=
+test $is_dry_run -eq 0 || try_run="echo "
+
+${try_run}mkdir -p "$BUILD_FOLDER" && ${try_run}cd "$BUILD_FOLDER" || die "Cannot access build directory $BUILD_FOLDER" $?
+
+test ${#PLATFORM} -eq 0 || GENERATOR="$GENERATOR -A $PLATFORM"
+
+test $skip_config -eq 1 || ${try_run}cmake -G $GENERATOR "$SCRIPTPATH" || exit $?
+test $skip_build -eq 1 || ${try_run}cmake --build . --target $TARGET || exit $?
+test $has_install -eq 0 || ${try_run}cmake --install . --prefix "$PREFIX" || exit $?
+
+if test $has_test -eq 1; then
+    LUA_CPATH="$("$PREFIX/bin/luajit" -e 'print(package.cpath)');$PREFIX/lib/lua/?.so"
+    LUA_CPATH="$LUA_CPATH" "$PREFIX/bin/luajit" "$SCRIPTPATH/test/test.lua"
+fi
